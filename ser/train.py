673a36a23939a739433b55817da762cc12dff2d9
abertrand <abertrand@C02FWYN7Q6L4.local>
2021-11-23 15:23:31 +0000

    corrections to params, data loading and model saving

--- ser/train.py
+++ ser/train.py
@@ -1,6 +1,8 @@
 import torch
 from torch import optim
 import torch.nn.functional as F
+from dataclasses import dataclass
+from datetime import datetime
 
 from ser.model import Net
 from ser.constants import PROJECT_ROOT, DATA_DIR, RESULTS_DIR
@@ -18,8 +20,15 @@ def my_train(name: str, ):
     learning_rate = 0.01
 
     # ----- TO-DO: save the parameters! -----
-    params = {'epochs': epochs, 'batch size': batch_size, 'learning rate': learning_rate}
-    filename =  RESULTS_DIR / "params.json"
+    @dataclass
+    class Params:
+        epochs: int 
+        bs: int
+        lr: int
+
+    params = Params(epochs, bs, lr)
+    timestamp = datetime.now(tz=None)
+    filename =  RESULTS_DIR / f"params_{name}_{timestamp}.json"
     with open(filename, 'w') as file_object:  
         json.dump(params, file_object)
 
@@ -33,11 +42,12 @@ def my_train(name: str, ):
     ts = get_transforms()
 
     # dataloaders
-    training_dataloader = load_data(directory="../data", download_bool=True, train_bool=True, ts=ts, batch_size=batch_size, shuffle_bool=True, num_workers=1)
+    training_dataloader = load_data(directory=DATA_DIR, download_bool=True, train_bool=True, ts=ts, batch_size=batch_size, shuffle_bool=True, num_workers=1)
     validation_dataloader = load_data(directory=DATA_DIR, download_bool=True, train_bool=False, ts=ts, batch_size=batch_size, shuffle_bool=False, num_workers=1)
   
 
     # train
+    val_acc_old = 0    # initialise val accuracy to save best model
     for epoch in range(epochs):
         for i, (images, labels) in enumerate(training_dataloader):
             images, labels = images.to(device), labels.to(device)
@@ -55,6 +65,7 @@ def my_train(name: str, ):
         # validate
         val_loss = 0
         correct = 0
+
         with torch.no_grad():
             for images, labels in validation_dataloader:
                 images, labels = images.to(device), labels.to(device)
@@ -70,4 +81,9 @@ def my_train(name: str, ):
                 f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
             )
 
+        if val_acc > val_acc_old:
+            directory = RESULTS_DIR / f'model_{name}_{timestamp}.pt'
+            torch.save(model, directory)
+
+        val_acc_old = val_acc
 
